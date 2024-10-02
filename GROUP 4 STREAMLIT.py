8900809fe74c672b3326b514f14d7e74122b9336
edelle-del <155860200+edelle-del@users.noreply.github.com>
2024-10-02 23:37:27 +0800

    Update GROUP 4 STREAMLIT.py

--- GROUP 4 STREAMLIT.py
+++ GROUP 4 STREAMLIT.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import altair as alt
+from wordcloud import WordCloud
+from mpl_toolkits.mplot3d import Axes3D
 
 st.title('Analyzing Laptops and Their Prices')
 
